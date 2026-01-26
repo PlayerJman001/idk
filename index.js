9a943b9e235e3590c1540d997ff4bff603f78795
Jimmy <jimmytwxu@gmail.com>
2026-01-26 14:37:09 +0000

    bleb

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const canvas = document.getElementById("ACanvas");
 const ctx = canvas.getContext("2d");
 
+ctx.beginpath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillstyle = "red";
+ctx.fill;
+ctx.closePath()
